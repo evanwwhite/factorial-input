a62e55e0a04c3ec86887955d9069fe71451a3596
Evan White <69662443+evanwwhite@users.noreply.github.com>
2020-11-20 13:22:07 -0600

    Add files via upload

---
+++ Factorials.java
@@ -0,0 +1,59 @@
+//Evan White
+import java.util.Scanner;
+public class Factorials {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		long factorial = 1;
+		int num1 = 0;
+	
+  System.out.println("Input a Number: ");
+ 
+     if(input.hasNextInt())
+ {
+	 num1 = input.nextInt();
+ }
+     else
+ {
+	 System.out.println("Try another input");
+	 input.close();
+	 return;
+ }
+ 
+  
+     for(int i = 1; i <= num1; i++)
+     {
+	  factorial = factorial * i;
+	  
+  }
+     System.out.println(factorial);
+ int num2 = 1;
+ int addNum = 1;
+ 
+while(addNum >= num2);
+{
+	
+}
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+	}
+
+}

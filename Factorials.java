7b9463aff1d917122b6da4326c5fc27d1b11049d
Evan White <69662443+evanwwhite@users.noreply.github.com>
2020-12-01 13:58:07 -0600

    Update Factorials.java

--- Factorials.java
+++ Factorials.java
@@ -8,7 +8,7 @@ public class Factorials {
 		long factorial = 1;
 		int num1 = 0;
 	
-  System.out.println("Input a Number: ");
+  System.out.println("Input a Number: " );
  
      if(input.hasNextInt())
  {
@@ -18,42 +18,44 @@ public class Factorials {
  {
 	 System.out.println("Try another input");
 	 input.close();
+	 
 	 return;
  }
  
-  
      for(int i = 1; i <= num1; i++)
      {
 	  factorial = factorial * i;
 	  
   }
      System.out.println(factorial);
- int num2 = 1;
- int addNum = 1;
+     System.out.println(" The For Loop : " +  factorial);
+     
+ factorial = 1;
  
-while(addNum >= num2);
+while(num1 > 1)
 {
+	factorial = factorial * num1;
 	
+	num1 = num1 - 1;
 }
  
-
+ System.out.println(" The While Loop : " + factorial);
  
+ do
+ {
+	 factorial = factorial* num1;
+	 num1 = num1 -1;
+ } while(num1 > 1);
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+ System.out.println(" The Do While Loop : " + factorial);
  
 	}
-
 }
+/*
+ Input a Number: 
+6
+720
+ A For Loop : 720
+ While Loop : 720
+ Do While Loop : 720
+*/
